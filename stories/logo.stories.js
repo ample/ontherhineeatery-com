3eed20f3311fe49337ef778b9cfc3987eb36bcb7
Maria Szubski <hello@mariaszubski.com>
2019-09-11 13:45:31 -0400

    Create brand Logo component

---
+++ stories/logo.stories.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+import { withKnobs, text } from "@storybook/addon-knobs"
+
+import GlobalStyles from "../src/components/global/styles"
+import { colors } from "../src/components/global/variables"
+
+import Logo from "../src/components/logo"
+import notes from "./__notes__/logo.md"
+
+const stories = storiesOf("Logo", module)
+
+stories.addDecorator(withKnobs)
+
+stories.addParameters({
+  backgrounds: [
+    { name: "white", value: colors.white },
+    { name: "gray700", value: colors.gray700, default: true },
+  ],
+})
+
+stories.add(
+  "Logo",
+  () => {
+    let color = text("Color", "#FFFFFF")
+    return (
+      <>
+        <GlobalStyles />
+        <Logo color={color} />
+      </>
+    )
+  },
+  { notes: notes }
+)
