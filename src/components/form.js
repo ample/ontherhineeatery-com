afb03a7fb92abcb61443b701bd75230ee54b692a
Sean C Davis <scdavis41@gmail.com>
2019-09-18 14:26:51 -0400

    Form component accepts and renders from a config object

--- src/components/form.js
+++ src/components/form.js
@@ -1,12 +1,52 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-const Form = ({ children, ...props }) => {
+import FormSelect from "./utilities/form-select"
+import Button from "./button"
+
+const Form = ({ config }) => {
+  const renderInput = opts => (
+    <input
+      type="text"
+      name={opts.name}
+      required={opts.required}
+      placeholder={opts.label}
+      className={opts.className}
+      key={opts.name}
+    />
+  )
+
+  const renderSelect = opts => (
+    <FormSelect options={opts.options} placeholder={opts.label} />
+  )
+
+  const renderSubmit = opts => <Button type="submit">{opts.label}</Button>
+
+  const renderTextarea = opts => (
+    <textarea placeholder={opts.label} rows={opts.rows || 5} />
+  )
+
+  const fieldMap = {
+    button: renderSubmit,
+    null: renderInput,
+    select: renderSelect,
+    string: renderInput,
+    submit: renderSubmit,
+    text: renderTextarea,
+    undefined: renderInput
+  }
+
   return (
-    <form action="" method="get" {...props}>
-      {children}
+    <form action="" method="get">
+      {config.map(f => fieldMap[f.type](f))}
     </form>
   )
 }
 
 export default Form
+
+Form.propTypes = {
+  config: PropTypes.object.isRequired
+}
+
+Form.defaultProps = {}
--- stories/form.stories.js
+++ stories/form.stories.js
@@ -19,12 +19,52 @@ stories.addDecorator(withKnobs)
 stories.add(
   "Form",
   () => {
-    const options = [
-      { value: "opt1", label: "Option 1" },
-      { value: "opt2", label: "Option 2" },
-      { value: "opt3", label: "Option 3" },
-      { value: "opt4", label: "Option 4" },
+    const formConfig = [
+      {
+        name: "name",
+        label: "Name",
+        required: true
+      },
+      {
+        name: "email",
+        label: "Email",
+        required: true,
+        className: "invalid"
+      },
+      {
+        required: true,
+        label: ".field-50 .col-left",
+        className: "field-50 col-left"
+      },
+      {
+        required: true,
+        label: ".field-50 .col-right",
+        className: "field-50 col-right"
+      },
+      {
+        name: "subject",
+        label: "Reason for inquiry",
+        required: true,
+        options: [
+          { value: "opt1", label: "Option 1" },
+          { value: "opt2", label: "Option 2" },
+          { value: "opt3", label: "Option 3" },
+          { value: "opt4", label: "Option 4" }
+        ],
+        type: "select"
+      },
+      {
+        name: "body",
+        label: "Short Message...",
+        required: true,
+        type: "text"
+      },
+      {
+        label: "Component Button",
+        type: "button"
+      }
     ]
+
     const invalid = boolean("Missing required field", true)
     return (
       <>
@@ -32,46 +72,8 @@ stories.add(
         <Container bgColor={g.colors.gray100}>
           <Row center="xs">
             <Col md={9} lg={7} xl={6}>
-
               <h3>Form Styles Demo</h3>
-
-              <Form>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  required
-                  placeholder={`Text ${invalid ? ".invalid" : ""}`}
-                  className={invalid ? "invalid" : ""}
-                />
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  required
-                  placeholder="Email"
-                />
-                <input
-                  type="text"
-                  required
-                  placeholder=".field-50 .col-left"
-                  className="field-50 col-left"
-                />
-                <input
-                  type="text"
-                  required
-                  placeholder=".field-50 .col-right"
-                  className="field-50 col-right"
-                />
-                <FormSelect
-                  options={options}
-                  placeholder="FormSelect Utility"
-                />
-                <textarea placeholder="Textarea" rows="5" />
-                <Button type="submit">
-                  Component Button
-                </Button>
-              </Form>
+              <Form config={formConfig} />
             </Col>
           </Row>
         </Container>
