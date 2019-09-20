c4cd8c563d6d4054a8f7263c6f88b18f97f153e1
Maria Szubski <hello@mariaszubski.com>
2019-09-20 16:14:13 -0400

    Set up favicon with gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,17 +16,14 @@ module.exports = {
         path: `${__dirname}/src/images`
       }
     },
+
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      }
+        short_name: 'On The Rhine Eatery',
+        theme_color: `#252422`,
+        icon: `src/images/favicon.png`,
+      },
     },
     {
       resolve: `gatsby-source-contentful`,
