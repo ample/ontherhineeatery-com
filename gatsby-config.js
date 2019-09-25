573568a9df5b6faed472bf2b112fe10a9a8c2758
Sean C Davis <scdavis41@gmail.com>
2019-09-25 12:54:37 -0400

    Add link catcher

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-catch-links`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
