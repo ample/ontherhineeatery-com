6dbc7b20cdbdb40aa5540fc5331e7b273a664130
Sean C Davis <scdavis41@gmail.com>
2019-09-30 10:27:03 -0400

    Add image support within content blocks

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,76 +1,20 @@
 import React from "react"
 import { graphql } from "gatsby"
-import styled from "styled-components"
-import { Row, Col } from "react-flexbox-grid"
-// import Img from "gatsby-image/withIEPolyfill"
 
-import { screen } from "../components/global/variables"
+// eslint-disable-next-line
+import PageAttributes from "../fragments/page-attributes"
 
+import Containers from "../components/containers"
 import Layout from "../components/layout"
-import GlobalStyles from "../components/global/styles"
-import Container from "../components/layout/container"
-
-const StyledHero = styled.div`
-  margin-bottom: 5rem;
-  img,
-  picture,
-  .gatsby-image-wrapper {
-    width: 100%;
-    height: 100%;
-    max-height: 38.2rem;
-    object-fit: cover;
-  }
-
-  @media ${screen.min.sm} and ${screen.max.md} {
-    img,
-    picture,
-    .gatsby-image-wrapper {
-      max-height: 26rem;
-    }
-  }
-
-  @media ${screen.max.sm} {
-    img,
-    picture,
-    .gatsby-image-wrapper {
-      width: auto;
-      height: 20rem;
-    }
-  }
-`
 
 const NotFoundPage = ({ data, ...props }) => (
   <Layout
     navMenus={data.navMenus.edges.map(n => n.node)}
     settings={data.settings.edges.map(n => n.node)}
+    page={data.page}
     location={props.location}
   >
-    <GlobalStyles />
-
-    <Container
-      className="text-center"
-      padding={{ desktop: "3.6rem", mobile: "3.6rem" }}
-    >
-      <Row center="md">
-        <Col lg={9}>
-          <StyledHero>
-            {/* <Img
-              fluid={props.hero.fluid}
-              objectFit="cover"
-              objectPosition="50% 50%"
-            /> */}
-          </StyledHero>
-        </Col>
-      </Row>
-      <Row center="md">
-        <Col md={9} lg={7} xl={6}>
-          <h3>Page Not Found</h3>
-          <p class="text-sm">
-            The page you're trying to access cannot be located.
-          </p>
-        </Col>
-      </Row>
-    </Container>
+    <Containers data={data.page.containers} />
   </Layout>
 )
 
@@ -78,6 +22,10 @@ export default NotFoundPage
 
 export const query = graphql`
   query NotFoundPageQuery {
+    page: contentfulPage(layout: { eq: "404" }) {
+      ...PageAttributes
+    }
+
     navMenus: allContentfulNavMenu {
       edges {
         node {
---
+++ stories/image/hero.stories.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+
+import * as g from "../../src/components/global/variables"
+
+import ImageHero from "../../src/components/image/hero"
+import data from "../__fixtures__/pages.json"
+import notes from "../__notes__/image/grid.md"
+
+const stories = storiesOf("Images", module)
+
+stories.addParameters({
+  backgrounds: [
+    { name: "white", value: g.colors.white, default: true },
+    { name: "robinegg", value: g.colors.robinegg }
+  ]
+})
+
+stories.add("Hero", () => <ImageHero image={data.edges[0].node.image} />, {
+  notes: notes
+})
