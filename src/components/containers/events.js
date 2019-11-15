49c200292fc3feaef374c354280869c59015211f
Maria Szubski <hello@mariaszubski.com>
2019-11-15 16:11:40 -0500

    Set up Event Containers

--- src/components/containers/events.js
+++ src/components/containers/events.js
@@ -1,8 +1,44 @@
 import React from "react"
+import styled from "styled-components"
+import { Row, Col } from "react-flexbox-grid"
 import { StaticQuery, graphql } from "gatsby"
-
+import Img from "gatsby-image/withIEPolyfill"
 // eslint-disable-next-line
 import EventAttributes from "../../fragments/event-attributes"
+
+import { screen } from "../global/variables"
+import Container from "../layout/container"
+import Link from "../utilities/link"
+import HTML from "../utilities/html"
+
+const StyledLink = styled(Link)`
+  display: block;
+  text-align: left;
+  text-decoration: none;
+  margin-bottom: 3.6rem;
+  &:hover h3,
+  &:focus h3 {
+    text-decoration: underline;
+  }
+
+  img,
+  picture,
+  .gatsby-image-wrapper {
+    height: 100%;
+    height: 24.6rem;
+  }
+
+  @media ${screen.max.md} {
+    margin-bottom: 5rem;
+    text-align: center;
+    h3 {
+      text-decoration: underline;
+    }
+    .gatsby-image-wrapper {
+      margin-bottom: 1.8rem;
+    }
+  }
+`
 
 const EventsContainer = props => (
   <StaticQuery
@@ -18,10 +54,38 @@ const EventsContainer = props => (
       }
     `}
     render={data => {
-      const events = data.events.edges.map(({ node }) => (
-        <h2 key={node.id}>{node.title}</h2>
+      const events = data.events.edges.map(({ node }, idx) => (
+        <StyledLink
+          to={node.permalink}
+          key={`special-event_${idx}`}
+          aria-label={`Event: ${node.title}. Description: ${node.subtitle}. ${node.body.body}`}
+        >
+          <Row>
+            {node.image && (
+              <Col md={5} xl={4}>
+                <Img
+                  fluid={node.image.fluid}
+                  objectFit="cover"
+                  objectPosition="50% 50%"
+                  aria-hidden={true}
+                />
+              </Col>
+            )}
+            <Col xs>
+              <h3>{node.title}</h3>
+              <h6>{node.subtitle}</h6>
+              {node.body && <HTML field={node.body} />}
+            </Col>
+          </Row>
+        </StyledLink>
       ))
-      return <>{events}</>
+      return (
+        <Container as="section" aria-label="Upcoming Special Events">
+          <Row center="xs">
+            <Col lg={9}>{events}</Col>
+          </Row>
+        </Container>
+      )
     }}
   />
 )
