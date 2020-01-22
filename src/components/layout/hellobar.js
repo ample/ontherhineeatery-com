68cfc7f3c88a74d4c2bf0565e3d1261f5709d934
Silas Baur <silasbaur@gmail.com>
2020-01-22 11:24:34 -0500

    Add hellobar script

---
+++ src/components/layout/hellobar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const HelloBarScript = () => {
+  return (
+    <script
+      src="https://my.hellobar.com/d370e2a6b32cc36c446b70f664d69d3d4f7974ae.js"
+      type="text/javascript"
+      async="async"
+    ></script>
+  )
+}
+
+export default HelloBarScript
