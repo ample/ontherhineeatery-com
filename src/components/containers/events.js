5ff2041f152682cee866ba22a05e9f05899ad2a0
Austin Engen <austin@helloample.com>
2020-03-04 15:42:11 -0500

    fix styles after review

--- src/components/containers/events.js
+++ src/components/containers/events.js
@@ -9,80 +9,28 @@ import { screen, colors } from "../global/variables"
 import Container from "../layout/container"
 import Link from "../utilities/link"
 import HTML from "../utilities/html"
-import WeeklyEventImage from "../../images/header_img-1@2x.png"
+
+const StyledImage = styled.div``
+
+const StyledDetails = styled.div``
+
+const StyledRow = styled(Row)``
 
 const OneColumnContainer = styled(Container)`
-  /* One column styles here ... */
-`
+  background-color: ${colors.gray200};
+  text-align: center;
+  padding: 8rem 1.5rem;
 
-const TwoColumnContainer = styled(Container)`
-  /* Two column styles here ... */
-
-  ${StyledRow} {
-    /* Nested styled row styles here ... */
-  }
-`
-
-const StyledRow = styled(Row)`
-  border-style: solid;
-  border-color: ${colors.gray300};
-  border-width: 0 0 1px 0;
-
-  &:last-of-type {
-    border-style: none;
-  }
-
-  .col-lg-9,
-  .col-md-6 {
-    padding-left: 16px;
-    padding-right: 16px;
-
+  > div {
     &:first-of-type {
-      > div {
-        position: relative;
+      width: 75%;
+      margin: auto;
 
-        :after {
-          background-color: ${colors.gray300};
-          position: absolute;
-          right: -15px;
-          height: 100%;
-          top: 0;
-          box-sizing: border-box;
-          border: 0.5px solid ${colors.gray300};
-
-          @media ${screen.max.md} {
-            border-style: none;
-          }
-        }
+      @media ${screen.max.md} {
+        width: 95%;
       }
     }
   }
-
-  .has-image {
-    align-self: center;
-  }
-
-  @media ${screen.max.md} {
-    border-style: none;
-    padding-bottom: 0;
-  }
-`
-
-const StyledImage = styled.div`
-  .gatsby-image-wrapper {
-    margin-top: 20px;
-    margin-bottom: 20px;
-
-    @media ${screen.max.md} {
-      height: 24rem;
-      margin-bottom: 0px;
-    }
-  }
-`
-
-const StyledDetails = styled.div`
-  text-align: left;
-  margin: 3rem 0;
 
   :after {
     content: "";
@@ -112,57 +60,148 @@ const StyledDetails = styled.div`
     }
   }
 
-  h3 {
-    margin-bottom: 1.5rem;
-  }
-
-  a,
-  a > h3 {
-    text-decoration: none;
-  }
-
-  a:hover > h3,
-  a:focus > h3 {
-    text-decoration: underline;
-  }
-
-  @media ${screen.max.md} {
-    img {
-      height: 100%;
-      width: 50%;
-      height: 12rem;
-    }
-
-    a > h3 {
-      text-decoration: underline;
-    }
-
-    .gatsby-image-wrapper {
-      margin-bottom: 1.8rem;
-    }
-  }
-`
-
-const StyledCol = styled(Col)`
   h2 {
     margin-bottom: 3rem;
   }
+
   h3 {
     margin-bottom: 1.5rem;
-    margin-top: 3rem;
-    @media ${screen.max.md} {
-      margin-top: 1.5rem;
+    margin-top: 1.5rem;
+  }
+
+  ${StyledImage} {
+    .gatsby-image-wrapper {
+      margin: 20px auto;
+      height: 25rem;
+      width: 30rem;
+
+      @media ${screen.max.md} {
+        height: 24rem;
+        margin-bottom: 0px;
+      }
     }
   }
-  img {
-    height: 250px;
-    width: auto;
+  ${StyledRow} {
+    padding-top: 2.5rem;
   }
 `
 
-const StyledEvents = styled.div`
-  background-color: ${colors.gray200};
-  padding-top: 3rem;
+const TwoColumnContainer = styled(Container)`
+  margin: 5rem 1.5rem;
+
+  ${StyledRow} {
+    border-style: solid;
+    border-color: ${colors.gray300};
+    border-width: 0 0 1px 0;
+
+    &:last-of-type {
+      border-style: none;
+    }
+
+    .col-lg-9,
+    .col-md-6 {
+      padding-left: 16px;
+      padding-right: 16px;
+
+      &:first-of-type {
+        > div {
+          position: relative;
+
+          :after {
+            background-color: ${colors.gray300};
+            position: absolute;
+            right: -15px;
+            height: 100%;
+            top: 0;
+            box-sizing: border-box;
+            border: 0.5px solid ${colors.gray300};
+
+            @media ${screen.max.md} {
+              border-style: none;
+            }
+          }
+        }
+      }
+    }
+
+    .has-image {
+      align-self: center;
+    }
+
+    @media ${screen.max.md} {
+      border-style: none;
+      padding-bottom: 0;
+    }
+  }
+
+  ${StyledDetails} {
+    text-align: left;
+    margin: 3rem 0;
+
+    :after {
+      content: "";
+    }
+
+    ul {
+      display: inline-block;
+      padding: 0;
+      text-align: left;
+
+      li {
+        display: inline;
+
+        :after {
+          content: "•";
+          letter-spacing: 1em;
+          background: center center no-repeat;
+          padding-left: 1rem;
+          white-space: nowrap;
+        }
+
+        :last-of-type {
+          :after {
+            content: " ";
+          }
+        }
+      }
+    }
+
+    h3 {
+      margin-bottom: 1.5rem;
+    }
+
+    a,
+    a > h3 {
+      text-decoration: none;
+    }
+
+    a:hover > h3,
+    a:focus > h3 {
+      text-decoration: underline;
+    }
+
+    @media ${screen.max.md} {
+      a > h3 {
+        text-decoration: underline;
+      }
+
+      .gatsby-image-wrapper {
+        margin-bottom: 1.8rem;
+      }
+    }
+  }
+
+  ${StyledImage} {
+    .gatsby-image-wrapper {
+      align-self: center;
+      margin: 3rem 0;
+
+      @media ${screen.max.md} {
+        height: 24rem;
+        margin-bottom: 0px;
+      }
+    }
+  }
 `
 
 const EventsContainer = props => (
@@ -240,39 +279,12 @@ const EventsContainer = props => (
         props.layout === "One Column" ? OneColumnContainer : TwoColumnContainer
 
       return (
-        <>
-          <ContainerTagName as="section" aria-label={props.title}>
-            {props.body && <HTML field={props.body} />}
-            <Row center="xs">
-              <Col lg={9}>{eventsHtml}</Col>
-            </Row>
-          </ContainerTagName>
-
-          <StyledEvents>
-            <Container>
-              <Row center="xs">
-                <StyledCol lg={9}>
-                  <h2>Weekly Events</h2>
-                  <img
-                    alt=""
-                    src={WeeklyEventImage}
-                    objectFit="cover"
-                    objectPosition="50% 50%"
-                    aria-hidden={true}
-                  />
-
-                  <h3>Sunday Brunch</h3>
-                  <h6>New American comfort food</h6>
-                  <p>
-                    Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-                    mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam
-                    eget risus varius blandit sit amet non magna.
-                  </p>
-                </StyledCol>
-              </Row>
-            </Container>
-          </StyledEvents>
-        </>
+        <ContainerTagName as="section" aria-label={props.title}>
+          {props.body && <HTML field={props.body} />}
+          <Row center="xs">
+            <Col lg={9}>{eventsHtml}</Col>
+          </Row>
+        </ContainerTagName>
       )
     }}
   />
